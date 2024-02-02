bad681de851cc9b52255449577edb1bd348db61e
amshal1 <panshal911@gmail.com>
2024-02-02 19:07:35 +0530

    database connected

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const Connect = require("./src/connect");
 const { app } = require("./src/main");
 const PORT = 5000 || process.env.PORT;
 
 app.listen(PORT, () => {
   console.info(`App is running on Port ${PORT}`);
+  Connect();
 });
