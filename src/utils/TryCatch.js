02acdd64c88817998b86c3c9ff480ef5648e2dda
amshal1 <panshal911@gmail.com>
2024-02-03 18:38:51 +0530

    Trycatch and logger logic changed

--- src/utils/TryCatch.js
+++ src/utils/TryCatch.js
@@ -1,6 +1,25 @@
+const logger = require("../utils/logger");
 const TryCatch = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch((err) => {
-    next(err);
-  });
+  try {
+    Promise.resolve(fn(req, res, next))
+      .then(() => {
+        // Log success status code if no error occurred
+        logger(req, res, false, "");
+      })
+      .catch((err) => {
+        // Log error status code if an error occurred
+        logger(req, null, err?.status, true, err?.message);
+        // Send the error as response
+        res
+          .status(err.status || 500)
+          .json({ error: err.message || "Internal Server Error" });
+      });
+  } catch (err) {
+    // Catch synchronous errors
+    logger(req, null, err?.status, true, err?.message);
+    res
+      .status(err.status || 500)
+      .json({ error: err.message || "Internal Server Error" });
+  }
 };
 module.exports = TryCatch;
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,15 +1,19 @@
-function logger(req, res) {
+function logger(req, res, statuscode, isError, message) {
   const date = new Date().toLocaleTimeString();
   const protocol = req.protocol;
   const host = req.hostname;
   const PORT = 5000 || process.env.PORT;
   const url = `${protocol}://${host}:${PORT}${req.url}`;
-  const status = res.statusCode;
+  const status = statuscode || res.statusCode;
   const type = req.method;
-  if (res?.error) {
+  if (!isError) {
     console.log(`${date} |`, `${type}`, `${url} -`, `\x1b[32m${status}\x1b[0m`);
+    if (message) {
+      console.log(`\x1b[32m${message}\x1b[0m`);
+    }
   } else {
     console.log(`${date} |`, `${type}`, `${url} -`, `\x1b[31m${status}\x1b[0m`);
+    console.log("Error:", `\x1b[31m${message}\x1b[0m`);
   }
 }
 
